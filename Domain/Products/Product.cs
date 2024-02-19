1245d85cebe62bfd8c7dcf4f2c8a853423819b44
Vitu <vitorcsharp@hotmail.com>
2024-02-19 15:13:02 -0300

    create class dbcontext and add properties and dbset

--- Domain/Products/Product.cs
+++ Domain/Products/Product.cs
@@ -3,6 +3,7 @@
     public class Product : Entity //Herança (como temos varios atributos iguais, foi melhor criar uma herança)
     {          
         public string Name { get; set; }
+        public int CategoryId { get; set; } //-> Chave estrangeira do Category (no banco ele vai criar com esse nome)
         public Category Category { get; set; }
         public string Description { get; set; }
         public bool HasStock { get; set; } 
