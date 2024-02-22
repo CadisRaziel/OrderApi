5930e6de7817060fad21e577b2ebd25826fc536c
Vitu <vitorcsharp@hotmail.com>
2024-02-22 15:14:06 -0300

    commit assembly and cache

--- obj/Debug/net6.0/OrderApi.AssemblyInfo.cs
+++ obj/Debug/net6.0/OrderApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dda3aa63217e3f86dd40905c0ac83c96572ec8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+229881f6edcd4504793b1a33d3377e2dfb7eeafd")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
